21452d30953a56c82e6569ce0f1bf9a038440599
jhsong2580 <thdwmdgns@naver.com>
2022-05-10 17:29:13 +0900

    feat: entity auditing 기능 사용을 위해 EnableJpaAudition Annotation추가

--- src/main/java/toy/epc/EpcApplication.java
+++ src/main/java/toy/epc/EpcApplication.java
@@ -2,8 +2,10 @@ package toy.epc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class EpcApplication {
 
 	public static void main(String[] args) {
