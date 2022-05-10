c8e27844afad50bd114c1e2a18ca4c0b5fa394fa
jhsong2580 <thdwmdgns@naver.com>
2022-05-10 11:10:32 +0900

    hello project

---
+++ src/main/java/toy/epc/README.md
@@ -0,0 +1,8 @@
+toy
+
+---
+
+- 자유롭게 의견낼수있는 게시판 
+- vm control 페이지 
+- 사용자는 master/user 두개로 분류
+  - spring security를 사용하고 jwt토큰을 통해 인증
---
+++ src/test/java/toy/epc/EpcApplicationTests.java
@@ -0,0 +1,13 @@
+package toy.epc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
